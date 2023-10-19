3f381a3bc5f6c1992809687e1e6cf2fb3255bcc2
Stans987 <148479255+Stans987@users.noreply.github.com>
2023-10-19 20:20:41 +0200

    Commit Stanislas

--- test2024/src/main/java/frc/robot/Robot.java
+++ test2024/src/main/java/frc/robot/Robot.java
@@ -53,6 +53,8 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {}
 
+  // WESH Stanislas
+
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
